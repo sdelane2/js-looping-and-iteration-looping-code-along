e911af85496045d33a8f64bbaf1bfec91ccb0205
sdelane2 <seanmartindelaney@gmail.com>
2020-11-15 18:00:00 -0500

    loops

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(array, name) {
+    let newArray = []
+    for (let i = 0; i < array.length; i++) {
+    newArray.push(`Thank you, ${array[i]}, for the wonderful ${name} gift!`)
+    }
+    return newArray
+}
+
+
+function countDown(num) {
+    while (num >= 0)
+    console.log(num--)
+}
+
